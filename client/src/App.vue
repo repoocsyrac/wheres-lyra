8dca51b6139edd8727fee2431021087df1e18863
syrac <carys.cooper@univ.ox.ac.uk>
2024-09-20 00:58:44 +0100

    Move websocket responsibility from SightingsList.vue to App.vue, and update graph in realtime also

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,7 +3,7 @@
     <h1>Where's Lyra?</h1>
     <img src="./assets/lyra.jpeg" alt="Lyra the cat" class="img-rounded">
     <ReportSighting />
-    <SightingsList />
+    <SightingsList :sightings-data="sightings" />
     <SightingsChart :sightings-data="sightings" />
     
   </div>
@@ -11,6 +11,7 @@
 
 <script>
   import axios from 'axios';
+  import { io } from "socket.io-client";
   import ReportSighting from './components/ReportSighting.vue';
   import SightingsList from './components/SightingsList.vue';
   import SightingsChart from './components/SightingsChart.vue';
@@ -25,6 +26,15 @@
       ReportSighting, SightingsList, SightingsChart
     },
     async mounted() {
+      // Connect to websocket 
+      const socket = io("http://127.0.0.1:5000");
+            
+      // Listen for new_sighting events
+      socket.on('new_sighting', (sighting) => {
+          console.log("New sighting received: ", sighting);
+          this.sightings.unshift(sighting); // Adds new sighting to the top of the list
+      });
+
       // Fetch sightings from backend
       this.getSightings();
     },
@@ -44,12 +54,12 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  background-color: #fefaf1;
+  background-color: #faf3e4;
   /*color: #2c3e50;
   margin-top: 60px;*/
 }
 
 html {
-  background-color: #fefaf1;
+  background-color: #faf3e4;
 }
 </style>
--- client/src/components/SightingsList.vue
+++ client/src/components/SightingsList.vue
@@ -2,7 +2,7 @@
     <div>
         <h2>Recent Sightings</h2>
         <ul class="list-unstyled">
-            <li v-for="sighting in sightings.slice(0,10)" :key="sighting.id">
+            <li v-for="sighting in sightingsData.slice(0,10)" :key="sighting.id">
                 {{ sighting.location }} - {{ new Date(sighting.timestamp).toLocaleString() }}
             </li>
         </ul>
@@ -10,35 +10,15 @@
 </template>
 
 <script>
-    import axios from 'axios';
-    import { io } from "socket.io-client";
-
     export default {
+        props: {
+            sightingsData: Array
+        },
+
         data() {
             return {
                 sightings: []
             };
-        },
-
-        mounted() {
-            // Connect to websocket 
-            const socket = io("http://127.0.0.1:5000");
-            
-            // Listen for new_sighting events
-            socket.on('new_sighting', (sighting) => {
-                console.log("New sighting received: ", sighting);
-                this.sightings.unshift(sighting); // Adds new sighting to the top of the list
-            });
-
-            // Fetch initial list of sightings
-            this.getSightings();
-        },
-
-        methods: {
-            async getSightings() {
-                const response = await axios.get('http://127.0.0.1:5000/api/sightings');
-                this.sightings = response.data;
-            }
         }
     };
 </script>
