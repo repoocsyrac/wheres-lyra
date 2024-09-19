8ff7a4f9cddc9e6a537f6b05a00c88fc29ba73f6
syrac <carys.cooper@univ.ox.ac.uk>
2024-09-20 00:01:23 +0100

    Limited number of past sightings shown to 10

--- client/src/components/SightingsList.vue
+++ client/src/components/SightingsList.vue
@@ -2,7 +2,7 @@
     <div>
         <h2>Recent Sightings</h2>
         <ul class="list-unstyled">
-            <li v-for="sighting in sightings" :key="sighting.id">
+            <li v-for="sighting in sightings.slice(0,10)" :key="sighting.id">
                 {{ sighting.location }} - {{ new Date(sighting.timestamp).toLocaleString() }}
             </li>
         </ul>
